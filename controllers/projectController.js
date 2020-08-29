cd57f1ad1bc153bba23a6882a84d4dd44f3adb68
Bob Nunes <51120356+bobnunes@users.noreply.github.com>
2020-08-29 12:24:03 -0300

    Atualização do projectController

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -14,7 +14,16 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.post('/add', async (req, res) => {
+router.get('/:projectId', async (req, res) => {
+    try {
+        const project = await Project.findById(req.params.projectId);
+        res.json(project);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+router.post('/', async (req, res) => {
     const project = new Project({
         name: req.body.name,
     });
@@ -29,5 +38,27 @@ router.post('/add', async (req, res) => {
     }
     
 });
+
+router.delete('/:projectId', async (req, res) => {
+    try {
+        const removeProject = await Project.remove({ _id: req.params.projectId });
+        res.json(removeProject);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+router.patch('/:projectId', async (req, res) => {
+    try {
+        const updateProject = await Project.updateOne(
+            { _id: req.params.projectId },
+            { $set: { name: req.body.name } }
+        );
+        res.json(updateProject);
+    } catch (err) {
+        res.json({ message: err })
+    }
+});
+
 
 module.exports = app => app.use('/project', router);
